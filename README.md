c2f6a309737ab499d89847a9df0122102915eb51
swaggp <mutebinuhu1@gmail.com>
2021-10-07 11:38:51 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# beach-football
+# beach
---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors:{
+        'container':'#F6F3D8',
+        'darktext': '#3E3E3E',
+        'search-place': '#01D98B',
+        'inner-container': '#FFFEF6'
+      }
+    },
+    fontFamily:{
+
+      heading: ['Avenir Next'],
+
+    }
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
